e5c1ec8c9097a63512741573e1da2e67f5318d40
Morteza Rohaninejad <mortezaroha@gmail.com>
2026-03-18 17:05:14 -0700

    Update survivor_engine.py

--- survivor_engine.py
+++ survivor_engine.py
@@ -18,7 +18,7 @@ from survivor_agent import (
 )
 from survivor_schedule import load_schedule_from_csv
 
-num_agents = 1000000
+num_agents = 500000
 num_contestants = 1000
 num_weeks = 18
 num_teams = 32
